540df6507328116ed90154560d90798e906ddf03
ahmedghounami <ahmedghounami0@gmail.com>
2025-05-30 17:16:24 +0100

    file structring controllers

---
+++ backend/controllers/registration.js
@@ -0,0 +1,60 @@
+async function getPasswordHash(password) {
+  const saltRounds = 10;
+  try {
+    const hash = await bcrypt.hash(password, saltRounds);
+    return hash;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+    throw new Error("Password hashing failed");
+  }
+}
+
+async function createuser(db, body) {
+  const { email, password, firstname, username, lastname } = body;
+  const hashingpassword = await getPasswordHash(password);
+  return new Promise((resolve, reject) => {
+    db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
+      if (err) return reject(err);
+
+      if (row) {
+        return reject(new Error("User already exists"));
+      }
+      db.run(
+        `INSERT INTO users (email, password, firstname, username, lastname) VALUES (?, ?, ?, ?, ?)`,
+        [email, hashingpassword, firstname, username, lastname],
+        function (err) {
+          if (err) {
+            reject(err);
+          } else {
+            console.log("User created");
+            resolve({ id: this.lastID, email, firstname, username, lastname });
+          }
+        }
+      );
+    });
+  });
+}
+
+function postUserHandler(db) {
+  return async (request, reply) => {
+    const { email, password, firstname, username, lastname } = request.body;
+
+    if (!email || !password || !firstname || !username || !lastname) {
+      return reply.code(400).send({ error: "Bad request: Missing fields" });
+    }
+    try {
+      const user = await createuser(db, request.body);
+      return reply.code(201).send({ message: "User created", user });
+    } catch (err) {
+      if (err.message === "User already exists") {
+        return reply.code(409).send({ error: err.message });
+      }
+      console.error(err.message);
+      return reply.code(500).send({ error: "Database error" });
+    }
+  };
+}
+
+module.exports = {
+  postUserHandler,
+};
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-
+const { postUserHandler } = require("../controllers/registration")
 const registration = {
   schema: {
     body: {
@@ -28,6 +28,7 @@ const registration = {
     },
   },
 };
+
 const login = {
   schema: {
     body: {
@@ -51,16 +52,7 @@ const login = {
   },
 };
 
-async function getPasswordHash(password) {
-  const saltRounds = 10;
-  try {
-    const hash = await bcrypt.hash(password, saltRounds);
-    return hash;
-  } catch (error) {
-    console.error("Error hashing password:", error);
-    throw new Error("Password hashing failed");
-  }
-}
+
 
 function getUserByEmailAndPassword(db, email) {
   return new Promise((resolve, reject) => {
@@ -71,51 +63,6 @@ function getUserByEmailAndPassword(db, email) {
   });
 }
 
-async function createuser(db, body) {
-  const { email, password, firstname, username, lastname } = body;
-  const hashingpassword = await getPasswordHash(password);
-  return new Promise((resolve, reject) => {
-    db.get("SELECT * FROM users WHERE email = ?", [email], (err, row) => {
-      if (err) return reject(err);
-
-      if (row) {
-        return reject(new Error("User already exists"));
-      }
-      db.run(
-        `INSERT INTO users (email, password, firstname, username, lastname) VALUES (?, ?, ?, ?, ?)`,
-        [email, hashingpassword, firstname, username, lastname],
-        function (err) {
-          if (err) {
-            reject(err);
-          } else {
-            console.log("User created");
-            resolve({ id: this.lastID, email, firstname, username, lastname });
-          }
-        }
-      );
-    });
-  });
-}
-
-function postUserHandler(db) {
-  return async (request, reply) => {
-    const { email, password, firstname, username, lastname } = request.body;
-
-    if (!email || !password || !firstname || !username || !lastname) {
-      return reply.code(400).send({ error: "Bad request: Missing fields" });
-    }
-    try {
-      const user = await createuser(db, request.body);
-      return reply.code(201).send({ message: "User created", user });
-    } catch (err) {
-      if (err.message === "User already exists") {
-        return reply.code(409).send({ error: err.message });
-      }
-      console.error(err.message);
-      return reply.code(500).send({ error: "Database error" });
-    }
-  };
-}
 
 function loginUserHandler(db) {
   return async (request, reply) => {
